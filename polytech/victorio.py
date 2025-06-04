3d115f08115882fe65fc32195f45393366b7d3ff
victorio-kalelle <kalelleeee@gmail.com>
2025-06-04 15:57:27 +0800

    add menu on victorio.py

---
+++ polytech/victorio.py
@@ -0,0 +1,61 @@
+import os
+
+UNSET_OPTION = 0
+EXIT_OPTION = 8
+
+def display_menu():
+    print("_________________________________")
+    print("  Victorio's information System  ")
+    print("_________________________________")
+    print("1. Basic Information")
+    print("2. Goals")
+    print("3. Alarm Clock")
+    print("4. Comment from Capilitan")
+    print("5. Comment from Niones")
+    print("6. Comment from Raquem")
+    print("7. Comment from Villarta")
+    print("8. Exit")
+    print("________________________________")
+
+def get_user_choice():
+    try:
+        return int(input("Enter choice: "))
+    except ValueError:
+        return UNSET_OPTION
+
+def display_get_choice(choice):
+    match choice:
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+        case 5:
+            pass
+        case 6:
+            pass
+        case 7:
+            pass
+        case _:
+            print("Invalid choice. Try again.")
+
+def kalelle():
+    while True:
+        display_menu()
+        choice = get_user_choice()
+
+        if choice == EXIT_OPTION:
+            print("Exiting the system.")
+            break
+
+        display_get_choice(choice)
+
+kalelle()
+
+
+
+
+
