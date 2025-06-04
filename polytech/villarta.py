fbc6375b7b0602a1aa209611b641ed8de362cc98
villarta-johnkeith <johnkeithvillarta3@gmail.com>
2025-06-04 16:29:18 +0800

    add villarta's skeleton menu

---
+++ polytech/villarta.py
@@ -0,0 +1,40 @@
+import os
+from colorama import Fore, Style, init
+
+# Initialize colorama
+init(autoreset=True)
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def display_header():
+    print(Fore.MAGENTA + Style.BRIGHT + "=" * 50)
+    print("🌟 Welcome to My Personal Info Menu! 🌟")
+    print("=" * 50 + Style.RESET_ALL)
+
+def display_menu():
+    display_header()
+    print(Fore.MAGENTA + "Please choose an option:")
+    print("1. Basic Information")
+    print("2. Goals")
+    print("0. Exit" + Style.RESET_ALL)
+
+def keith():
+    while True:
+        clear_screen()
+        display_menu()
+        choice = input(Fore.MAGENTA + "\nEnter your choice: " + Style.RESET_ALL)
+        
+        if choice == '1':
+            print(Fore.MAGENTA + "You selected: Basic Information." + Style.RESET_ALL)
+        elif choice == '2':
+            print(Fore.MAGENTA + "You selected: Goals." + Style.RESET_ALL)
+        elif choice == '0':
+            print(Fore.MAGENTA + "Exiting..." + Style.RESET_ALL)
+            break
+        else:
+            print(Fore.RED + "Invalid choice. Please try again." + Style.RESET_ALL)
+
+        input("Press Enter to continue...")
+
+keith()
