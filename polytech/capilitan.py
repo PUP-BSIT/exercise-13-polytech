ac6cc49fae579d0e1056d340af887069efa13f81
capilitan_mikee <mikee.capilitan@gmail.com>
2025-06-04 16:30:18 +0800

    update capilitan menu

--- polytech/capilitan.py
+++ polytech/capilitan.py
@@ -4,13 +4,13 @@ from colorama import Fore, Style, init
 init(autoreset=True)
 
 def display_header():
-    print(Fore.LIGHTMAGENTA_EX + Style.BRIGHT + "=" * 50)
-    print(" 🩷🌺 Welcome to Mikee Capilitan's Menu! 🌺🩷")
+    print(Fore.LIGHTBLUE_EX + Style.BRIGHT + "=" * 50)
+    print("    💙🦋  Mikee Capilitan's Menu  🦋💙")
     print("=" * 50 + Style.RESET_ALL)
 
 def display_menu():
     display_header()
-    print(Fore.LIGHTMAGENTA_EX + "Please choose an option:")
+    print(Fore.LIGHTBLUE_EX + "Please choose an option:")
     print("1. Basic Information")
     print("2. Goals")
     print("3. Exit" + Style.RESET_ALL)
@@ -19,27 +19,27 @@ def mikee():
     while True:
         display_menu()
         choice = input(
-            Fore.LIGHTMAGENTA_EX +
+            Fore.LIGHTBLUE_EX +
             "\nEnter your choice: " +
             Style.RESET_ALL
         )
         if choice == '1':
             print(
-                Fore.LIGHTMAGENTA_EX +
-                "Basic Information." +
+                Fore.LIGHTBLUE_EX +
+                "You selected Basic Information." +
                 Style.RESET_ALL
             )
             input("Press Enter to continue...")
         elif choice == '2':
             print(
-                Fore.LIGHTMAGENTA_EX +
-                "Goals." +
+                Fore.LIGHTBLUE_EX +
+                "You selected Goals." +
                 Style.RESET_ALL
             )
             input("Press Enter to continue...")
         elif choice == '3':
             print(
-                Fore.LIGHTMAGENTA_EX +
+                Fore.LIGHTBLUE_EX +
                 "Exiting..." +
                 Style.RESET_ALL
             )
@@ -51,5 +51,5 @@ def mikee():
                 Style.RESET_ALL
             )
             input("Press Enter to continue...")
-            
+
 mikee()
---
+++ polytech/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+
+            )
+            input("Press Enter to continue...")
+
+mikee()
