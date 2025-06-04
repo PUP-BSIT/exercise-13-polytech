9558a0279be2684830a5cea24f639a84e663cbd7
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-06-04 16:05:49 +0800

    add choices processing in menu

--- polytech/raquem.py
+++ polytech/raquem.py
@@ -16,40 +16,81 @@ def display_menu():
     print(Fore.MAGENTA + "Please choose an option:")
     print("1. Basic Information")
     print("2. Goals")
-    print("3. Exit" + Style.RESET_ALL)
+    print("3. Option 3")
+    print("4. Option 4")
+    print("5. Option 5")
+    print("6. Option 6")
+    print("7. Option 7")
+    print("0. Exit" + Style.RESET_ALL)
 
-def annie():
-    while True:
-        clear_screen()
-        display_menu()
-        choice = input(
-            Fore.MAGENTA + "\nEnter your choice: " + Style.RESET_ALL
-        )
-        if choice == '1':
+def display_basic_info():
+    print(Fore.MAGENTA + "Basic Information goes here." + Style.RESET_ALL)
+
+def display_goals():
+    print(Fore.MAGENTA + "Goals go here." + Style.RESET_ALL)
+
+def option_3():
+    print(Fore.MAGENTA + "Option 3." + Style.RESET_ALL)
+
+def option_4():
+    print(Fore.MAGENTA + "Option 4." + Style.RESET_ALL)
+
+def option_5():
+    print(Fore.MAGENTA + "Option 5." + Style.RESET_ALL)
+
+def option_6():
+    print(Fore.MAGENTA + "Option 6." + Style.RESET_ALL)
+
+def option_7():
+    print(Fore.MAGENTA + "Option 7." + Style.RESET_ALL)
+
+def process_choice(choice):
+    clear_screen()
+    match choice:
+        case 1:
+            display_basic_info()
+        case 2:
+            display_goals()
+        case 3:
+            option_3()
+        case 4:
+            option_4()
+        case 5:
+            option_5()
+        case 6:
+            option_6()
+        case 7:
+            option_7()
+        case 0:
             print(
                 Fore.MAGENTA +
-                "You selected Basic Information." +
+                "Thank you for using this menu! Goodbye!" +
                 Style.RESET_ALL
             )
-            # Placeholder for processing code
-            input("Press Enter to continue...")
-        elif choice == '2':
-            print(
-                Fore.MAGENTA +
-                "You selected Goals." +
-                Style.RESET_ALL
-            )
-            # Placeholder for processing code
-            input("Press Enter to continue...")
-        elif choice == '3':
-            print(Fore.MAGENTA + "Exiting..." + Style.RESET_ALL)
-            break
-        else:
+        case _:
             print(
                 Fore.RED +
                 "Invalid choice. Please try again." +
                 Style.RESET_ALL
             )
-            input("Press Enter to continue...")
+
+def annie():
+    while True:
+        clear_screen()
+        display_menu()
+        try:
+            choice = int(
+                input(
+                    Fore.MAGENTA +
+                    "\nEnter your choice: " +
+                    Style.RESET_ALL
+                )
+            )
+        except ValueError:
+            choice = -1
+        process_choice(choice)
+        if choice == 0:
+            break
+        input("Press Enter to continue...")
 
 annie()
