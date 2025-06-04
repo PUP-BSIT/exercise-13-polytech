e081bf491c81b75ccc9cc69c3743d3d571a95e62
niones-zyrajoy <nioneszyrajoy@gmail.com>
2025-06-04 16:25:41 +0800

    add choices processing in menu

--- polytech/niones.py
+++ polytech/niones.py
@@ -18,40 +18,28 @@ def display_menu():
     print("2. Goals")
     print("3. Exit" + Style.RESET_ALL)
 
+def process_choice(choice):
+    clear_screen()
+    match choice:
+        case '1':
+            print(Fore.CYAN + "\nYou selected Basic Information." + Style.RESET_ALL)
+            print("Display basic information here.")
+        case '2':
+            print(Fore.CYAN + "\nYou selected Goals." + Style.RESET_ALL)
+            print("Display goals here.")
+        case '3':
+            print(Fore.CYAN + "\nExiting..." + Style.RESET_ALL)
+        case _:
+            print(Fore.RED + "\nInvalid choice. Please try again." + Style.RESET_ALL)
+
 def zyra():
     while True:
         clear_screen()
         display_menu()
-        choice = input(
-            Fore.CYAN + "\nEnter your choice: " + Style.RESET_ALL
-        )
-        if choice == '1':
-            clear_screen()
-            print(
-                Fore.CYAN +
-                "\nYou selected Basic Information." +
-                Style.RESET_ALL
-            )
-            print("Display basic information here.")
-            input("\nPress Enter to continue...")
-        elif choice == '2':
-            clear_screen()
-            print(
-                Fore.CYAN +
-                "\nYou selected Goals." +
-                Style.RESET_ALL
-            )
-            print("Display goals here.")
-            input("\nPress Enter to continue...")
-        elif choice == '3':
-            print(Fore.CYAN + "\nExiting..." + Style.RESET_ALL)
+        choice = input(Fore.CYAN + "\nEnter your choice: " + Style.RESET_ALL)
+        process_choice(choice)
+        if choice == '3':
             break
-        else:
-            print(
-                Fore.RED +
-                "\nInvalid choice. Please try again." +
-                Style.RESET_ALL
-            )
-            input("Press Enter to continue...")
+        input(Fore.CYAN + "\nPress Enter to continue..." + Style.RESET_ALL)
 
 zyra()
