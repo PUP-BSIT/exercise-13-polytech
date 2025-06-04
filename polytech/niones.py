e6d8d7b216b73468db8a020fe2125d6162b45670
niones-zyrajoy <nioneszyrajoy@gmail.com>
2025-06-04 16:37:42 +0800

    fix choices processing

--- polytech/niones.py
+++ polytech/niones.py
@@ -16,21 +16,71 @@ def display_menu():
     print(Fore.CYAN + "Please choose an option:")
     print("1. Basic Information")
     print("2. Goals")
-    print("3. Exit" + Style.RESET_ALL)
+    print("3. Option 3")
+    print("4. Option 4")
+    print("5. Option 5")
+    print("6. Option 6")
+    print("7. Option 7")
+    print("0. Exit" + Style.RESET_ALL)
+
+def display_basic_info():
+    print(Fore.CYAN + "Basic Information goes here." + Style.RESET_ALL)
+
+
+def display_goals():
+    print(Fore.CYAN + "Goals go here." + Style.RESET_ALL)
+
+
+def option_3():
+    print(Fore.CYAN + "Option 3." + Style.RESET_ALL)
+
+
+def option_4():
+    print(Fore.CYAN + "Option 4." + Style.RESET_ALL)
+
+
+def option_5():
+    print(Fore.CYAN + "Option 5." + Style.RESET_ALL)
+
+
+def option_6():
+    print(Fore.CYAN + "Option 6." + Style.RESET_ALL)
+
+
+def option_7():
+    print(Fore.CYAN + "Option 7." + Style.RESET_ALL)
+
 
 def process_choice(choice):
     clear_screen()
     match choice:
-        case '1':
-            print(Fore.CYAN + "\nYou selected Basic Information." + Style.RESET_ALL)
-            print("Display basic information here.")
-        case '2':
-            print(Fore.CYAN + "\nYou selected Goals." + Style.RESET_ALL)
-            print("Display goals here.")
-        case '3':
-            print(Fore.CYAN + "\nExiting..." + Style.RESET_ALL)
+        case 1:
+            display_basic_info()
+        case 2:
+            display_goals()
+        case 3:
+            option_3()
+        case 4:
+            option_4()
+        case 5:
+            option_5()
+        case 6:
+            option_6()
+        case 7:
+            option_7()
+        case 0:
+            print(
+                Fore.CYAN +
+                "Thank you and Goodbye!" +
+                Style.RESET_ALL
+            )
         case _:
-            print(Fore.RED + "\nInvalid choice. Please try again." + Style.RESET_ALL)
+            print(
+                Fore.CYAN +
+                "Invalid choice. Please try again." +
+                Style.RESET_ALL
+            )
+
 
 def zyra():
     while True:
